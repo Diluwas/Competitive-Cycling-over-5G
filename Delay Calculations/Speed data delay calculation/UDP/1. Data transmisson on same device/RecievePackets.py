19d2855df05cb8156b3e5c8da6519dbf7a6783b1
Dilun Sandeepa <dilunsandeepa97fr@gmail.com>
2023-02-26 13:12:31 +0530

    UDP transmission for Two devices at same network

---
+++ Delay Calculations/Speed data delay calculation/UDP/1. Data transmisson on same device/RecievePackets.py
@@ -0,0 +1,8 @@
+import socket
+myIP = "192.168.1.102"
+myPort = 5005
+socket_reciver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_reciver.bind((myIP, myPort))
+while True:
+    data, addr = socket_reciver.recvfrom(1024)
+    print("recieved message :", data.decode(), addr)
