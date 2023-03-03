9aff33900bfa1759ccffe834501808e4beb6f71e
Dilun Sandeepa <dilunsandeepa97fr@gmail.com>
2023-03-03 08:23:47 +0530

    MQTT pulisher and subsrciber for two devices in same network

---
+++ Delay Calculations/Speed data delay calculation/MQTT/1. Data transmisson on same device/MQTT_subscriber.py
@@ -0,0 +1,16 @@
+import time
+import paho.mqtt.client as paho
+broker="test.mosquitto.org"
+client= paho.Client("client-002")
+
+print("connecting to broker ",broker)
+client.connect(broker, 1883)
+
+def on_message(client, userdata, message):
+    time.sleep(1)
+    print("received message =",str(message.payload.decode("utf-8")))
+
+
+client.on_message=on_message
+client.subscribe("CCO5/test")
+client.loop_forever()
